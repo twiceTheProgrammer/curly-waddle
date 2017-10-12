9f75d67bde2d4c54645c7ae338a9419d4a534018
twiceTheProgrammer <32726834+twiceTheProgrammer@users.noreply.github.com>
2017-10-12 06:29:30 +0200

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner ; 
+
+public class hello {
+      public static void main(String[] args){
+          Scanner keyboard = new Scanner(System.in);
+          System.out.println("What is your name ? ");
+          String name = keyboard.nextLine();
+          
+          System.out.println("Hello " + name);
+      }
+{
